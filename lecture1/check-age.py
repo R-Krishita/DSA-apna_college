e4ecbc94192801b06d240c9e478ebf26d3f24108
RKrishita <krishitaravat12345@gmail.com>
2024-12-29 18:08:04 +0530

    converted python code to cpp

--- lecture1/check-age.py
+++
@@ -1,5 +0,0 @@
-age = int(input("Enter your age = "))
-if(age>=18):
-    print("You ar eligible to get a driving license")
-else:
-    print("You are not eligible to get a driving license")
--- lecture1/simple-interest.py
+++
@@ -1,5 +0,0 @@
-p = int(input("Enter Principal rate = "))
-r = int(input("Enter Rate of interest = "))
-t = int(input("Enter Time in years= "))
-si = (p*r*t)/100
-print("Simple Interest = ", si)
